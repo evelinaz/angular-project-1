145be98c15d3d19c793cd9ae7bc2b41fa53c4970
Evelina <evelina.zobina@gmail.com>
2023-03-31 20:43:20 +0300

    lisa kassid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-project-1';
+
+  koikKassid = [
+		{ nimi: 'Puss', hind: 5, värv: 'must', pildiURL: '/mustkass.webp' },
+		{ nimi: 'Boots', hind: 10, värv: 'valge', pildiURL: '/valgekass.webp' },
+		{ nimi: 'Funny', hind: 20, värv: 'must', pildiURL: '/mustkass.webp' },
+		{ nimi: 'Bunny', hind: 25, värv: 'valge', pildiURL: '/valgekass.webp' },
+		{ nimi: 'Foxy', hind: 35, värv: 'orans', pildiURL: '/fox.jpeg' }
+	];
 }
